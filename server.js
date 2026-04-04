7cbfbb3ae35097a8c73930209e78b4408313b605
Nitesh-Codez <kushwahnitesh388@gmail.com>
2026-04-04 15:37:01 +0530

    api   mistake  done  add cutomer

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/", authRoutes);
-app.use("/", customerRoutes);
+app.use("/api", customerRoutes);
 
 app.get("/", (req, res) => {
   res.send("Purity Production API Running");
